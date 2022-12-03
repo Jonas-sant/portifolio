84243a2f4e9158fb97ed69e942a94c4908a43813
Jonas Santos <jonas.santos9708@gmail.com>
2022-12-03 20:45:37 -0300

    Fix: Estilização do formulário de contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,19 +14,26 @@ function Contact(){
             
             <form className="form" onSubmit={()=>{}}>
                 
+                <h5>Nome</h5>
                 <input className="input" type="text" placeholder="Digite seu nome" onChange={(e)=>setName(e.target.value)} value={name}/>
 
+                                
+                <h5>E-mail</h5>
                 <input className="input" type="text" placeholder="Digite seu E-mail" onChange={(e)=>setEmail(e.target.value)} value={email} />
 
+                <h5>Watsapp</h5>
                 <input className="input" type="text" placeholder="Digite seu Whatsapp" onChange={(e)=>{setWhatsapp(e.target.value)}} value={whatsapp}/>
                 <span className="spanWatsapp">Caso tenha whatsapp, informe para ficar mais fácil o contato.</span>
 
+                <h5>Menssagem</h5>
                 <textarea className="textarea" placeholder="Digite sua Menssagem" onChange={(e)=>{setMessage(e.target.value)}} value={message}>
 
                 </textarea>    
 
+                <input type="submit" className="sendButton" value="Enviar"/>
 
             </form>
+            
         </section>
     );
 
